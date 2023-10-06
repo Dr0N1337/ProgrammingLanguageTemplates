5a5835293765b707e4c1a406f1b6407ebd15ddb8
Dr0N <Dr0N@DESKTOP-AK38JUI>
2023-10-06 22:30:56 +0300

    #2 сделал код

---
+++ Templates/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "../TemplateList/SingleList.h"
+
+using namespace rut::miit;
+int main()
+{
+	SingleList<int> list1;
+	list1.pushBack(4);
+	list1.pushBack(7);
+	list1.pushFront(6);
+	list1.pushBack(9);
+	std::cout << list1.toString() << "\t";
+	SingleList<int> list2;
+	list2 = list1;
+
+	std::cout << list2.toString();
+	return 0;
+}
